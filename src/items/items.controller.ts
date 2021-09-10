13e609e081007b4019c4ed20965929c97389965e
tony <antolisah@gmail.com>
2021-09-10 09:34:01 +0300

    Post request route

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'This action returns all items';
+  }
+
+  @Post()
+  create(): string {
+    return 'This action creates an item';
   }
 }

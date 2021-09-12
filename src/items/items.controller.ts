7fb74274c1fe11e6870ca145d699f27fa0703ea7
tony <antolisah@gmail.com>
2021-09-12 22:10:35 +0300

    create items model

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -16,18 +16,18 @@ export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll(): Promise<Item[]> {
+  async findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<Item> {
+  async findOne(@Param('id') id: string): Promise<Item> {
     return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
 
   @Delete(':id')
